592c102b62fa9f4bed1220d53ffd93a4fcca360e
ivan-skukan <is53961@fer.hr>
2024-03-17 00:10:21 +0100

    getting there

--- Semesters/6sem/srs/labs/lab1/cyphertesting.py
+++ Semesters/6sem/srs/labs/lab1/cyphertesting.py
@@ -1,7 +1,8 @@
 from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Hash import SHA256, HMAC, SHA1
-
+import base64
+"""
 key = b'0123456789012345'
 
 iv = Random.get_random_bytes(AES.block_size) 
@@ -19,3 +20,43 @@ aesdecrypt = AES.new(key, AES.MODE_CFB, iv=iv)
 plaintext = aesdecrypt.decrypt(ciphertext)
 
 print(plaintext.decode('utf-8'))
+
+hmac = HMAC.new(key, digestmod=SHA256)
+hmac.update(msg)
+print(hmac.hexdigest())
+encrypt = aes.encrypt_and_digest(msg)
+"""
+
+iv = Random.get_random_bytes(AES.block_size)
+print(iv) 
+print(str(iv))
+
+key = b'0123456789012345'
+msg = "message".encode('utf-8')
+
+aes = AES.new(key, AES.MODE_GCM,  nonce=iv)
+
+ciphertext, tag = aes.encrypt_and_digest(msg)
+
+#print(ciphertext)
+
+aesdecrypt = AES.new(key, AES.MODE_GCM, nonce=iv)
+
+decrypt = aesdecrypt.decrypt_and_verify(ciphertext, tag)
+
+#print(decrypt.decode('utf-8'))
+
+with open('test.txt', 'w') as file:
+    #file.write(ciphertext.)
+    pass
+
+print()
+iv = base64.b64encode(iv).decode('utf-8')
+print(iv)
+line = base64.b64encode(b'|').decode('utf-8')
+big = iv + '|' + iv
+
+iv = big.split('|')[0]
+iv = base64.b64decode(iv)
+print(iv)
+
--- Semesters/6sem/srs/labs/lab1/pwmanager.py
+++ Semesters/6sem/srs/labs/lab1/pwmanager.py
@@ -1,93 +1,170 @@
+#!/usr/bin/env python3
 import os
 import sys
 from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Hash import SHA256, HMAC
 from Crypto.Protocol.KDF import PBKDF2
-
-#ideja: masterpassword za kdf kljuc?
-#kako cu cuvati masterpassword?
+import base64
 #cemu moze pristupiti napadac?
 #sha za hashiranje
 #mac za integritet
-#pbkdf2 za kljucž
-#iv?
+#pbkdf2 za kljuc
 
 def initIVSalt():
     iv = Random.get_random_bytes(AES.block_size)
     salt = Random.get_random_bytes(16)
     return iv, salt
 
-def help():
-    print("\nCommands:") 
-    print("\tinit <master_password> - initializes the master password - required for all other commands related to passwords. Can be called only once.")
-    print("\tput <master_password> <service> <password> - adds a new password for a service")
-    print("\tget <master_password> <service> - retrieves a password for a service")
-    print("\tlist <master_password> - lists all services for which a password is stored") #dodaj ako stignes
-    print("\treset - deletes all stored passwords including the master password") 
-    print("\texit - exits the program")
-
-
 def main():
     path = 'passwords.txt'
-    #args = sys.argv[1:]
-
-    if not os.path.exists(path):
-        with open(path, 'w') as file:
-            file.write("")
-
-    myMap = {} 
+    args = sys.argv[1:]
+    aes = None
     iv = salt = ""
-
-    print("\nThe password manager is running. Check docs for more info.")       
-    print("Write 'help' for a list of commands.")
-    print("Write 'exit' to exit the program.\n")
-    while True:
-        args = input("Enter command: ").split(" ")
-        if args[0] == "exit":
-            break
-        elif args[0] == "help":
-            help()
-        elif args[0] == "reset":
-            if os.path.getsize(path) == 0:
-                print("There are no stored passwords.")
-                continue
-            print("Are you sure you want to reset all stored passwords? (Y/N)")
-            answer = input()
-            if answer == "Y" or answer == "y":
-                with open(path, 'w') as file:
-                    file.write('') #clear the file
+    if args[0] == "reset":
+        if os.path.getsize(path) == 0:
+            print("There are no stored passwords.")
+            return
+        print("Are you sure you want to reset all stored passwords? (Y/N)")
+        answer = input()
+        if answer == "Y" or answer == "y":
+            with open(path, 'w') as file:
+                file.write('') #clear the file
                 print("All stored passwords have been deleted.")
-            else:
-                print("Operation canceled.")
-        elif args[0] == "init":
-            if len(args) != 2:
-                print("Invalid number of arguments.")
-                continue
-            mp = args[1] #master password
-            #provjeri da li je vec inicijaliziran????
-            #vjerojatno samo overwriteaj
+        else:
+            print("Operation canceled.")
+    elif args[0] == "init":
+        if len(args) != 2:
+            print("Invalid number of arguments.")
+            return
+        if not os.path.getsize(path) == 0:
+            print("Master password has already been initialized.")
+            return
+        mp = args[1] #master password
+
             #generiraj stvari idk
-            iv, salt = initIVSalt()
-            key = PBKDF2(mp, salt, dkLen=32)
+        iv, salt = initIVSalt()
+        padding = Random.get_random_bytes(32)
+        key = PBKDF2(mp, salt, dkLen=32, count=100000, prf=lambda p,s: HMAC.new(p,s,SHA256).digest())
+        aes = AES.new(key, AES.MODE_GCM, nonce=iv)
 
-            with open(path, 'r') as file:
-                file.write("iv: " + iv + "\n")
-                file.write("salt: " + salt + "\n")
+        encrypted, tag = aes.encrypt_and_digest(padding)
+        base64enc = base64.b64encode(encrypted).decode('utf-8')
+        base64tag = base64.b64encode(tag).decode('utf-8')
+        #base64line = base64.b64encode(b'|').decode('utf-8')
 
-        elif args[0] == "put":
-            if len(args) != 4:
-                print("Invalid number of arguments.")
-                continue
-            mp = args[1]
-            if True: #check if mp is correct    
-                pass
-            if iv == "": #vjv krivo(mozda ne), treba zamjenit sa provjerom mp
-                print("Master password has not been initialized.")
-                continue    
+        iv = base64.b64encode(iv).decode('utf-8')
+        salt = base64.b64encode(salt).decode('utf-8')
+
+        #bigstring = iv + '|' + salt + encrypted + '|' + tag
+        bigstring = iv + '|' + salt + '|' + base64enc + '|' + base64tag
+        #hmac = HMAC.new(mp, digestmod=SHA256)
+        #hmac.update(mp)
+
+        with open(path, 'w') as file:
+            file.write(bigstring)
+
+
+    elif args[0] == "put":
+        if len(args) != 4:
+            print("Invalid number of arguments.")
+            return
+        
+        mp = args[1]
+        site = args[2]
+        password = args[3]
+
+        with open(path, 'r') as file:
+            bigstring = file.read().split('|')
+            #bigstring = base64.b64decode(bigstring).decode('utf-8')
+            iv = bigstring.split('|')[0]
+            iv = base64.b64decode(iv) #i think
+            salt = bigstring.split('|')[1]
+            salt = base64.b64decode(salt)
+            encrypted = bigstring.split('|')[2]
+            encrypted = base64.b64decode(encrypted)
+            tag = bigstring.split('|')[3]
+            tag = base64.b64decode(tag)
+
+            #iv = base64.b64decode(iv)
+            #salt = base64.b64decode(salt)
+            #encrypted = base64.b64decode(encrypted)
+            #tag = base64.b64decode(tag)
+            #TU SAM STAOOO!!!!!!!!!!!!!!!!!!!!!!!!!
+            key = PBKDF2(mp, salt, dkLen=32, count=100000, prf=lambda p,s: HMAC.new(p,s,SHA256).digest())
+            aes = AES.new(key, AES.MODE_GCM, nonce=iv)
+
+            try:
+                decrypted = aes.decrypt_and_verify(encrypted, tag)
+            except ValueError:
+                print("Master password is incorrect or integrity of the file has been compromised.")
+                return    
             
-#=========================================================================            
-    print("Exiting the program.")
+            decrypted = decrypted.decode('utf-8')
+
+            if site in decrypted:
+                start = decrypted.find(site) + len(site) + 1
+                end = decrypted.find('|', start)
+                oldpass = decrypted[start:end]
+                decrypted = decrypted.replace(oldpass, password)
+            else:
+                decrypted += "|" + site + "," + password
+            encrypted, tag = aes.encrypt_and_digest(decrypted.encode('utf-8'))
+            #base64enc = base64.b64encode(encrypted).decode('utf-8')
+            #base64tag = base64.b64encode(tag).decode('utf-8')
+            #base64line = base64.b64encode(b'|').decode('utf-8')
+
+            iv, salt = initIVSalt()
+            #iv = base64.b64encode(b"iv:"+iv).decode('utf-8')
+            #salt = base64.b64encode(b"salt:"+salt+b"|").decode('utf-8')
+
+            #bigstring = iv + base64line + salt + base64enc + base64line + base64tag
+            bigstring = base64.b64encode(iv).decode('utf-8') + '|' + base64.b64encode(salt).decode('utf-8') + base64.b64encode(encrypted).decode('utf-8') + '|' + base64.b64encode(tag).decode('utf-8')
+            bigstring = base64.b64encode(bigstring.encode('utf-8')).decode('utf-8')
+
+            with open(path, 'w') as file:
+                file.write(bigstring)
+
+    elif args[0] == "get":
+        if len(args) != 3:
+            print("Invalid number of arguments.")
+            return
+        mp = args[1]
+        site = args[2]
+
+        with open(path,'r'):
+            bigstring = file.read()
+            iv = bigstring.split('|')[0].split(':')[1]
+            salt = bigstring.split('|')[1].split(':')[1]
+            encrypted = bigstring.split('|')[2]
+            tag = bigstring.split('|')[3]
+
+            iv = base64.b64decode(iv)
+            salt = base64.b64decode(salt)
+            encrypted = base64.b64decode(encrypted)
+            tag = base64.b64decode(tag)
+
+            key = PBKDF2(mp, salt, dkLen=32, count=100000, prf=lambda p,s: HMAC.new(p,s,SHA256).digest())
+            aes = AES.new(key, AES.MODE_GCM, nonce=iv)
+
+            try:
+                decrypted = aes.decrypt_and_verify(encrypted, tag)
+            except ValueError:
+                print("Master password is incorrect or integrity of the file has been compromised.")
+                return    
+            
+            decrypted = decrypted.decode('utf-8')
+
+            if site in decrypted:
+                start = decrypted.find(site) + len(site) + 1
+                end = decrypted.find('|', start)
+                password = decrypted[start:end]
+                print(password)
+            else:
+                print("Site not found.")
+    else:
+        print("Invalid command.")
 
 if "__main__" == __name__:
-    main()
+        main()
+    

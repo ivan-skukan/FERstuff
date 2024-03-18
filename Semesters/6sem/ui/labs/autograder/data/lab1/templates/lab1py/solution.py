c6bcfdd74129bbe3404040e1cbfe7fd726255064
ivan-skukan <is53961@fer.hr>
2024-03-18 11:11:58 +0100

    uilab1

--- Semesters/6sem/ui/labs/autograder/data/lab1/templates/lab1py/solution.py
+++ Semesters/6sem/ui/labs/autograder/data/lab1/templates/lab1py/solution.py
@@ -1,2 +1,41 @@
+import sys
+import os
 
-print("Hello AI")
+class Node:
+    def __init__(self,state,neighbours):
+        self.state = state
+        self.neighbours = neighbours
+    
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__))) #privremeno, mozda izbrisem
+
+print(os.getcwd()) 
+
+def main():
+    args = sys.argv[:1]
+    alg = args[1]
+    file = args[2]
+    heur = args[3]
+
+    path = '../../files/'
+
+    os.chdir(path)
+
+    try:
+        with open(file, 'r') as state_file:
+            lines = state_file.readlines()
+    except FileNotFoundError:
+        print(f"File not found,exiting")
+        return
+    
+    start = lines[0]
+    goal = lines[1]
+
+    for line in lines[2:]:
+        parent_state = line.split(':')[0]
+        neighbour_info = line.split(':')[1]
+
+
+if __name__ == "__main__":
+    main()

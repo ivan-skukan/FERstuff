be66a45adbcff27de1fe76d9424abe55db1660c8
ivan-skukan <is53961@fer.hr>
2024-05-28 16:57:35 +0200

    lab4 finito vise manje

--- Semesters/6sem/ui/labs/autograder/data/lab4/templates/lab4py/solution.py
+++ Semesters/6sem/ui/labs/autograder/data/lab4/templates/lab4py/solution.py
@@ -5,15 +5,54 @@ from math import e
 import random
 from neuralnet import NN
 
-def fitness(err):
-    return 1/err
+
 
 def crossover(p1, p2):
-    d1 = p1.copy()
-    d2 = p2.copy()
+    d = p1.copy()
+    L = len(p1.layers)
 
-    
+    for i in range(1,L):
+        key_w = 'w' + str(i)
+        key_b = 'b' + str(i)
 
+        weights_p1 = p1.weights[key_w]
+        weights_p2 = p2.weights[key_w]
+        biases_p1 = p1.weights[key_b]
+        biases_p2 = p2.weights[key_b]
+
+        #weights_d = d.weights[key_w]
+        #biases_d = d.weights[key_b]
+
+        average_w = [[(a+b)/2 for a,b in zip(row_p1,row_p2)] for row_p1,row_p2 in zip(weights_p1,weights_p2)]
+        average_b = [(a+b)/2 for a,b in zip(biases_p1,biases_p2)]
+
+        d.weights[key_w] = average_w
+        d.weights[key_b] = average_b
+
+    return d
+
+def mutate(d,p,K):
+     L = len(d.layers)
+
+     for i in range(1,L):
+        key_w = 'w' + str(i)
+        key_b = 'b' + str(i)
+
+        d.weights[key_w] = [[element + random.gauss(0, K) if random.random() <= p else element for element in row] for row in d.weights[key_w]]
+        d.weights[key_b] = [element + random.gauss(0, K) if random.random() <= p else element for element in d.weights[key_b]]
+
+def select_parent(P,evals):
+    total = sum(evals)
+    probabilities = [fitness/total for fitness in evals]
+
+    cumulative_probability = 0
+    threshold = random.random()
+
+    for i,p in enumerate(P):
+        cumulative_probability += probabilities[i]
+        if cumulative_probability > threshold:
+            return P
+    return P[-1]
 def generate_population(popsize, input_length, hidden_layers):
     P = []
     for _ in range(popsize):
@@ -48,13 +87,13 @@ def main():
     parse_args(parser)
     args = parser.parse_args()
 
-    #features_train,data_train = parse_file(args.train)
-    #_,data_test = parse_file(args.test)
+    features_train,data_train = parse_file(args.train)
+    _,data_test = parse_file(args.test)
 
-    #input_length = len(data_train[0])
-    input_length = 2
-    #hidden_layers = args.nn.split('s')
-    hidden_layers = [3,2]
+    input_length = len(data_train[0])
+    #input_length = 2
+    hidden_layers = args.nn.split('s')
+    #hidden_layers = [3,2]
     hidden_layers = [int(layer) for layer in hidden_layers if layer]
 
     #nn = NN(input_size=input_length, hidden_layers=hidden_layers)
@@ -62,31 +101,35 @@ def main():
     #print(nn.layers)
 
     P = generate_population(args.popsize, input_length, hidden_layers)
-    evals = []
-    for p in P:
-        evals.append(fitness(p.evaluate(data_train)))
+    evals = [p.evaluate() for p in P]
+    evals.sort()
+    #evals.sort()
+    P.sort(key=lambda p: p.last_eval)
 
     for iter in range(args.iter):
-        new_P = []
+        new_P = set() #set?!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        for i in range(args.elitism):
+            #new_P.add(P.pop())
+            new_P.add(P[-(i+1)])
         for i in range(args.popsize):
-            # Select two best parents
-            p1 = P.pop(P.index(max(evals)))
-            evals.pop(evals.index(max(evals)))
-            p2 = P.pop(P.index(max(evals)))
-            evals.pop(evals.index(max(evals)))
+            p1 = select_parent(P,evals)
+            P.remove(p1)
+            p2 = select_parent(P,evals)
             # Crossover using mean
-            d1,d2 = crossover(p1,p2)
-            # Mutation using Gaussian noise
-            new_P.append(d1)
-            new_P.append(d2)
-        
+            d = crossover(p1,p2)
+
+            mutate(d,args.p,args.K)
+
+            new_P.add(d)
+
         P = new_P
-        evals = []
-        for p in P:
-            evals.append((fitness(p.evaluate(data_train),p)))
+        evals = [p.evaluate() for p in P]
         #find the best while size < elitism ??
         if iter % 2000 == 0:
             print(f'[Train error @{iter}]: {max(evals)}')
+
+    
 
 
 
